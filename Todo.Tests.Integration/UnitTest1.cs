a68c14df44359f0a187a869e7e8a0bc41a7541b0
Euclydes Uchoas Dos Santos Neto <euclydes.uchoas@gmail.com>
2026-07-31 14:45:53 -0300

    Adicionar arquivos de projeto.

---
+++ Todo.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Todo.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
