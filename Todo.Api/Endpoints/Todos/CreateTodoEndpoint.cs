065e88ef95c92ecf744dcef6064670c7a0934cc6
Euclydes Uchoas Dos Santos Neto <euclydes.uchoas@gmail.com>
2026-08-03 22:31:57 -0300

    feat(api): add todo endpoint group and create todo endpoint

---
+++ Todo.Api/Endpoints/Todos/CreateTodoEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using Todo.Application.Abstractions.Messaging;
+using Todo.Application.Common.Results;
+using Todo.Application.Todos.CreateTodo;
+
+namespace Todo.Api.Endpoints.Todos;
+
+public sealed class CreateTodoEndpoint : IEndpoint<TodoEndpointGroup>
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/", async (CreateTodoRequest request, IServiceHandler<CreateTodoRequest, Guid> handler, CancellationToken cancellationToken) =>
+        {
+            var result = await handler.HandleAsync(request, cancellationToken);
+
+            return result.IsSuccess
+                ? Results.Created((string?)null, result)
+                : Results.BadRequest(result.Base);
+        })
+        .WithName("CreateTodo")
+        .Produces<Result<Guid>>(StatusCodes.Status201Created)
+        .Produces<Result>(StatusCodes.Status400BadRequest);
+    }
+}
---
+++ Todo.Api/Endpoints/Todos/TodoEndpointGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.Api.Endpoints.Todos;
+
+public sealed class TodoEndpointGroup : IEndpointGroup<TodoEndpointGroup>
+{
+    public RouteGroupBuilder MapEndpointGroup(IEndpointRouteBuilder app)
+    {
+        return app.MapGroup("v1/todos")
+            .WithTags(EndpointTags.Todos);
+    }
+}
